eaae1ce41a64758c6b2656261c1b5ead4bfcc9fb
한진희 <0200134hjh@gmail.com>
2025-11-15 05:17:50 +0900

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -7,14 +7,12 @@ procedure Main is
    Source : String := "";
    File   : File_Type;
 begin
-   -- Read example.coba
    Open(File, In_File, "example.coba");
 
    while not End_Of_File(File) loop
       declare
          L : constant String := Get_Line(File);
       begin
-         -- Safe string accumulation
          Source := Source & ASCII.LF & L;
       end;
    end loop;
@@ -22,8 +20,8 @@ begin
    Close(File);
 
    declare
-      Tokens : constant Lexer.Token_List   := Lexer.Tokenize(Source);
-      Prog   : constant Parser.Program_Type := Parser.Parse_Program(Tokens);
+      Tokens : constant Lexer.Token_List := Lexer.Tokenize(Source);
+      Prog   : constant AST.Program     := Parser.Parse_Program(Tokens);
    begin
       Runtime.Exec(Prog);
    end;

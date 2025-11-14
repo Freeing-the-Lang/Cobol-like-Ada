2e7d218f2e223d97fd5dfffccd207592773256e3
한진희 <0200134hjh@gmail.com>
2025-11-15 05:13:55 +0900

    Update parser.ads

--- src/parser.ads
+++ src/parser.ads
@@ -3,6 +3,10 @@ with AST;
 
 package Parser is
 
-   function Parse_Program(Tokens : Lexer.Token_List) return AST.Program;
+   -- Public alias type for AST.Program
+   type Program_Type is new AST.Program;
+
+   -- Main entry
+   function Parse_Program(Tokens : Lexer.Token_List) return Program_Type;
 
 end Parser;

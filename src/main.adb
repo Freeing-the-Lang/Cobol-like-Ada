9b79c98ea78c371da1b60010b5101f4dcdadf6a8
한진희 <0200134hjh@gmail.com>
2025-11-15 05:19:24 +0900

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -1,11 +1,13 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;              use Ada.Text_IO;
+with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
 with Lexer;
 with Parser;
 with Runtime;
+with AST;  -- ★★ 핵심! visibility 에러 해결
 
 procedure Main is
-   Source : String := "";
-   File   : File_Type;
+   Source_UB : Unbounded_String := To_Unbounded_String("");
+   File      : File_Type;
 begin
    Open(File, In_File, "example.coba");
 
@@ -13,15 +15,17 @@ begin
       declare
          L : constant String := Get_Line(File);
       begin
-         Source := Source & ASCII.LF & L;
+         -- ★ 안전한 방식 (경고 없음)
+         Source_UB := Source_UB & ASCII.LF & L;
       end;
    end loop;
 
    Close(File);
 
    declare
-      Tokens : constant Lexer.Token_List := Lexer.Tokenize(Source);
-      Prog   : constant AST.Program     := Parser.Parse_Program(Tokens);
+      Source : constant String             := To_String(Source_UB);
+      Tokens : constant Lexer.Token_List   := Lexer.Tokenize(Source);
+      Prog   : constant AST.Program        := Parser.Parse_Program(Tokens);
    begin
       Runtime.Exec(Prog);
    end;
